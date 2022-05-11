05e4a03d0e5e406e2d235befeb28ecd5a82cc09c
Sarabjit Singh <sarabjitsingh23@gmail.com>
2022-05-11 21:29:13 +0530

    fix

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-
+ 
 const name = 'Sarabjit Singh';
 export const siteTitle = 'Next.js Sample Website';
 
